280d737a29d2ba602ea9fffb80c528cecd139aa8
EmekaEkwo <chukwuemekaekwo@abbey.bac.edu>
2025-11-23 18:28:31 +1100

    Complete remaining taste tracker problems and tidy code

--- script.js
+++ script.js
@@ -79,37 +79,46 @@ console.log("Long food names:", longFoodNames);
 
 
 // 8. Create another array shortFoodNames for foods 6 characters or shorter.
+let shortFoodNames = [];
+
+for (let i = 0; i < friendFavorites.length; i++) {
+  const food = friendFavorites[i];
+  if (food.length <= 6) {
+    shortFoodNames.push(food);
+  }
+}
+
+console.log("Short food names:", shortFoodNames);
 
 
 
 // 9. Print both arrays and compare:
 // "There are more long-named foods." OR "There are more short-named foods."
+console.log("Long food names:", longFoodNames);
+console.log("Short food names:", shortFoodNames);
 
-const myFoods = ["tacos", "pizza", "sushi"];
-const popularFoods = ["burger", "pizza", "pasta", "ramen"];
-
-if (myFoods.length > popularFoods.length) {
-  console.log("My list is longer.");
-} else if (myFoods.length < popularFoods.length) {
-  console.log("Popular list is longer.");
+if (longFoodNames.length > shortFoodNames.length) {
+  console.log("There are more long-named foods.");
+} else if (longFoodNames.length < shortFoodNames.length) {
+  console.log("There are more short-named foods.");
 } else {
-  console.log("Both lists have the same length.");
+  console.log("Both groups have the same number of foods.");
 }
+
 
 // 10. STRETCH: Find the longest food name and print:
 // "The longest food name in the list is ______ with ___ characters."
-const searchFood = "pizza";
-let found = false;
+let longestFood = "";
+let longestLength = 0;
 
-for (let i = 0; i < favoriteFoods.length; i++) {
-  if (favoriteFoods[i] === searchFood) {
-    found = true;
+for (let i = 0; i < friendFavorites.length; i++) {
+  const food = friendFavorites[i];
+
+  if (food.length > longestLength) {
+    longestFood = food;
+    longestLength = food.length;
   }
 }
 
-if (found) {
-  console.log(searchFood + " is in the list.");
-} else {
-  console.log(searchFood + " is not in the list.");
-}
+console.log("The longest food name in the list is " + longestFood + " with " + longestLength + " characters.");
 
